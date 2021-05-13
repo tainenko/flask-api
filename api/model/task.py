f557a3e9083dfd9a8c03e7e88e0a9dc7dfd3a642
ko tainen <TonyK@hyvesolutions.com>
2021-05-14 01:28:30 +0800

    remove uniq contrain from Task name

--- api/model/task.py
+++ api/model/task.py
@@ -4,5 +4,5 @@ from api.model import db
 class Task(db.Model):
     __tablename__ = 'tasks'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, unique=True, nullable=False)
+    name = db.Column(db.String, nullable=False)
     status = db.Column(db.Integer, nullable=False)
